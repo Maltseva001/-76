17981872a36375c8b0c3c317d0c0ba95eb5df12b
Теори Накубуру <nakuburu@yandex.ru>
2021-01-24 22:15:47 +0200

    Добавьте файлы проекта.

---
+++ Строки/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            text = text.Replace(" ", string.Empty);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
